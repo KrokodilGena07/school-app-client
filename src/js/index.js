06b8db71d4c9c883a4cc6bf12e5a19f1dbaaed43
KrokodilGena07 <pasha.fenev@gmail.com>
2025-12-11 23:48:38 +0300

    update logic

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+const news = document.getElementById('news');
+
+fetch('http://localhost:5000/api/posts')
+    .then(data => {
+        let html = '';
+        console.log(data.json())
+        news.innerHTML = html;
+    })
